ee7e38cd987e7d8b518efcc5b1f4e985fdf30dea
Bookstore Admin <admin@bookstore.com>
2026-07-01 13:26:46 +0800

    添加 mangum 适配 Vercel

--- api/index.py
+++ api/index.py
@@ -3,4 +3,7 @@ import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "backend"))
 os.chdir(os.path.join(os.path.dirname(__file__), "..", "backend"))
 
-from app import app as handler
+from app import app
+from mangum import Mangum
+
+handler = Mangum(app)
